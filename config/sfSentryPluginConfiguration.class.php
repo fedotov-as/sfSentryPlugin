a35cd651a5e6e860967513a4df144829ef1ef388
Aleksandr Fedotov <mezonix@gmail.com>
2020-05-16 22:10:32 +0300

    Add capture functions

--- config/sfSentryPluginConfiguration.class.php
+++ config/sfSentryPluginConfiguration.class.php
@@ -20,20 +20,22 @@ class sfSentryPluginConfiguration extends sfPluginConfiguration
             $dsn = sfConfig::get('sentry_client_dsn');
             $options = sfConfig::get('sentry_client_options', array());
 
-            sfSentry::getInstance()->init($dsn, $options);
+            sfSentry::getInstance()->configure($dsn, $options);
             $this->dispatcher->connect('application.throw_exception', array($this, 'listenToExceptions'));
         }
     }
 
     /**
      * Listener to exceptions
+     *
      * @param sfEvent $event
+     *
      * @throws Exception
      */
     public function listenToExceptions(sfEvent $event)
     {
         $instance = sfSentry::getInstance();
-        if (false === $instance->isAvailable()) {
+        if (false === $instance->getStatus()) {
             return;
         }
         $instance->handleException($event->getSubject());
--- lib/sfSentry.class.php
+++ lib/sfSentry.class.php
@@ -2,10 +2,17 @@
 
 /**
  * Class sfSentry
+ *
  * @author Aleksandr Fedotov <mezonix@gmail.com>
  */
 class sfSentry
 {
+    const DEBUG = Raven_Client::DEBUG;
+    const INFO = Raven_Client::INFO;
+    const WARN = Raven_Client::WARN;
+    const ERROR = Raven_Client::ERROR;
+    const FATAL = Raven_Client::FATAL;
+
     /**
      * @var sfSentry
      */
@@ -14,15 +21,21 @@ class sfSentry
     /**
      * @var bool
      */
-    private $available = false;
+    private $status = false;
+
+    /**
+     * @var sfSentryClient
+     */
+    private $client;
 
     /**
      * @var Raven_ErrorHandler
      */
-    protected $errorHandler;
+    private $errorHandler;
 
     /**
      * Retrieves the singleton instance of this class.
+     *
      * @return sfSentry
      */
     static public function getInstance()
@@ -36,41 +49,82 @@ class sfSentry
     /**
      * @return bool
      */
-    public function isAvailable()
+    public function getStatus()
     {
-        return $this->available;
+        return (bool)$this->status;
     }
 
     /**
-     * @param string $dsn
-     * @param array $options
+     * @param bool $status
      */
-    public function init($dsn, $options)
+    private function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * Configure Sentry instance
+     *
+     * @param string $dsn     Data source name
+     * @param array  $options Configuration options
+     */
+    public function configure($dsn, $options)
     {
         if (empty($dsn)) {
-            $this->available = false;
+            $this->setStatus(false);
             return;
         }
-        $client = new sfSentryClient($dsn, $options);
-        $this->errorHandler = new Raven_ErrorHandler($client);
+        $this->client = new sfSentryClient($dsn, $options);
+        $this->errorHandler = new Raven_ErrorHandler($this->client);
         $this->errorHandler->registerExceptionHandler();
         $this->errorHandler->registerErrorHandler(true, E_ALL | E_STRICT);
         $this->errorHandler->registerShutdownFunction(1024);
-        $this->available = true;
+        $this->setStatus(true);
     }
 
     /**
      * Handler Exception
+     *
      * @param Exception $e
-     * @param bool $isError
-     * @param mixed $vars
+     * @param array     $vars
+     *
      * @throws Exception
      */
-    public function handleException($e, $isError = false, $vars = null)
+    public function handleException($e, $vars = null)
     {
-        if (false === $this->isAvailable()) {
+        if (false === $this->getStatus()) {
             return;
         }
-        $this->errorHandler->handleException($e, $isError, $vars);
+        $this->errorHandler->handleException($e, false, $vars);
+    }
+
+    /**
+     * Log a message to sentry
+     *
+     * @param string $message The message (primary description) for the event.
+     * @param array  $params  Params to use when formatting the message.
+     * @param string $level   Log level group
+     * @param mixed  $vars    User variables
+     */
+    public function captureMessage($message, $params = array(), $level = self::ERROR, $vars = null)
+    {
+        if (false === $this->getStatus()) {
+            return;
+        }
+        $this->client->captureMessage($message, $params, $level, false, $vars);
+    }
+
+    /**
+     * Log an exception to sentry
+     *
+     * @param Exception $exception The Throwable/Exception object.
+     * @param mixed     $vars      Log level group
+     */
+    public function captureException($exception, $vars = null)
+    {
+        if (false === $this->getStatus()) {
+            return;
+        }
+        $this->client->captureException($exception, null, null, $vars);
     }
 }
